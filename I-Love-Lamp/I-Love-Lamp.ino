314c7950ae57c2a1d5678901a2c7efe7884309c5
Carson Book <carson.book@gravitylab.com>
2018-05-31 22:47:38 -0700

    added spectrum analyzer setup and reading code; added knob control for number of waves;

--- I-Love-Lamp/I-Love-Lamp.ino
+++ I-Love-Lamp/I-Love-Lamp.ino
@@ -5,7 +5,13 @@
 //Constants
 #define LED_PIN   A5  //Pin for the pixel strand. Does not have to be analog.
 #define KNOB_PIN  A1  //Pin for the trimpot 10K
-#define LED_TOTAL 120  //Change this to the number of LEDs in your strand.
+#define LED_TOTAL 60  //Change this to the number of LEDs in your strand.
+
+//Declare Spectrum Shield pin connections
+#define STROBE 4
+#define RESET 5
+#define DC_One A0
+#define DC_Two A1 
 
 //Globals
 Adafruit_NeoPixel strand = Adafruit_NeoPixel(LED_TOTAL, LED_PIN, NEO_GRB + NEO_KHZ800);  //LED strand object
@@ -16,7 +22,15 @@ long interval = 100;           // interval at which to blink (milliseconds)
 
 float knob = 1023.0;   //Holds the percentage of how twisted the trimpot is. Used for adjusting the max brightness.
 
+int waves = 2;
+
 bool goTime = false;
+
+//Define spectrum variables
+int freq_amp;
+int Frequencies_One[7];
+int Frequencies_Two[7]; 
+int i;
 
 //Standard Functions
 void setup() {    //Like it's named, this gets ran before any other function.
@@ -29,7 +43,27 @@ void setup() {    //Like it's named, this gets ran before any other function.
   Serial.begin(9600); //Sets data rate for serial data transmission.
   Serial.println("Setup RainbowSine");
   strand.begin(); //Initialize the LED strand object.
-  strand.show();  //Show a blank strand, just to get the LED's ready for use. 
+  strand.show();  //Show a blank strand, just to get the LED's ready for use.
+
+  //Set spectrum Shield pin configurations
+  pinMode(STROBE, OUTPUT);
+  pinMode(RESET, OUTPUT);
+  pinMode(DC_One, INPUT);
+  pinMode(DC_Two, INPUT);  
+  digitalWrite(STROBE, HIGH);
+  digitalWrite(RESET, HIGH);
+  
+  //Initialize Spectrum Analyzers
+  digitalWrite(STROBE, LOW);
+  delay(1);
+  digitalWrite(RESET, HIGH);
+  delay(1);
+  digitalWrite(STROBE, HIGH);
+  delay(1);
+  digitalWrite(STROBE, LOW);
+  delay(1);
+  digitalWrite(RESET, LOW);
+  
 }
 
 SIGNAL(TIMER0_COMPA_vect) {
@@ -38,6 +72,9 @@ SIGNAL(TIMER0_COMPA_vect) {
 
 
 void loop() {
+
+    knob = analogRead(KNOB_PIN) / 1023.0; //Record how far the trimpot is twisted
+    waves = knobToFactors120(); 
   
   if (goTime) {
     goTime= false;
@@ -46,6 +83,10 @@ void loop() {
     if(currentMillis - previousMillis > interval) {
       previousMillis = currentMillis;   
       run();     
+      
+      Read_Frequencies();
+//      Graph_Frequencies();
+      
     }
   
   }
@@ -53,15 +94,32 @@ void loop() {
 
 
 void run() {
-//  float breathe = ((sin(position*2*3*PI/180)+1)/2*0.8)+0.2;
-  float breathe = 1;
+  float breathe = ((sin(position*2*3*PI/180)+1)/2*0.4)+0.2;
+  int freak[7];
   
+  for( i= 0; i<7; i++) {
+     if(Frequencies_Two[i] > Frequencies_One[i]){
+        freak[i] = Frequencies_Two[i];
+     }
+     else{
+        freak[i] = Frequencies_One[i];
+     }
+   }
+
+//  freak [6] = 1023;
+    
   for (int i = 0; i < LED_TOTAL; i++) {
     
-    int waves = 4;
     int r = (int)((sin(i*waves*3*PI/180)+1)*255/2);
     int g = (int)((sin(i*waves*3*PI/180+2*PI/3)+1)*255/2);
     int b = (int)((sin(i*waves*3*PI/180+4*PI/3)+1)*255/2);
+    
+//    int r = (int)((sin(i*waves*3*PI/180)+1)*255/2 * freak[0]/1023);
+//    int r = 0;
+//    int g = (int)((sin(i*waves*3*PI/180+2*PI/3)+1)*255/2 * freak[2]/1023);
+//    int g = 0;
+//    int b = (int)((sin(i*waves*3*PI/180+4*PI/3)+1)*255/2 * freak[1]/1023);
+//    int b = 0;
     
 //    strand.setPixelColor(((i) % LED_TOTAL), strand.Color(r*breathe,g*breathe,b*breathe));
     strand.setPixelColor(((i + position) % LED_TOTAL), strand.Color(r*breathe,g*breathe,b*breathe));
@@ -74,3 +132,48 @@ void run() {
   strand.show();
 }
 
+/*******************Pull frquencies from Spectrum Shield********************/
+void Read_Frequencies(){
+  //Read frequencies for each band
+  for (freq_amp = 0; freq_amp<7; freq_amp++)
+  {
+    Frequencies_One[freq_amp] = analogRead(DC_One);
+    Frequencies_Two[freq_amp] = analogRead(DC_Two); 
+    digitalWrite(STROBE, HIGH);
+    digitalWrite(STROBE, LOW);
+  }
+}
+
+/*******************Light LEDs based on frequencies*****************************/
+void Graph_Frequencies(){
+   for( i= 0; i<7; i++)
+   {
+     if(Frequencies_Two[i] > Frequencies_One[i]){
+        Serial.println(Frequencies_Two[i]);
+     }
+     else{
+        Serial.println(Frequencies_One[i]);
+     }
+   }
+}
+
+// this function takes the knob value and returns a factor of 120
+// which ensures there is always an equal number of off LEDs between on LEDs
+uint8_t knobToFactors120() {
+  if (knob > (float)15/(float)16) return 120;
+  if (knob > (float)14/(float)16) return 60;
+  if (knob > (float)13/(float)16) return 40;
+  if (knob > (float)12/(float)16) return 30;
+  if (knob > (float)11/(float)16) return 24;
+  if (knob > (float)10/(float)16) return 20;
+  if (knob > (float)9/(float)16) return 15;
+  if (knob > (float)8/(float)16) return 12;
+  if (knob > (float)7/(float)16) return 10;
+  if (knob > (float)6/(float)16) return 8;
+  if (knob > (float)5/(float)16) return 6;
+  if (knob > (float)4/(float)16) return 5;
+  if (knob > (float)3/(float)16) return 4;
+  if (knob > (float)2/(float)16) return 3;
+  if (knob > (float)1/(float)16) return 2;
+  return 1;
+}
